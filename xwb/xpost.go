d32a335acbe3b9dda806c812f612e85528a4d082
xu2016 <451511298@qq.com>
2018-10-12 10:20:22 +0800

    l

--- xwb/xpost.go
+++ xwb/xpost.go
@@ -7,23 +7,36 @@ import (
 	"net/url"
 )
 
-type InfoJSON struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-}
+const (
+	//PostSuc XPost成功
+	PostSuc = iota
+	//PostERR Post提交失败
+	PostERR
+	//ReadERR 读取HTML内容失败
+	ReadERR
+	//UJsonERR 解析JSON失败
+	UJsonERR
+)
 
 //XPost post方法提交
-func XPost(urlstr, qstr, qcstr, qtstr string) (body []byte, err error) {
+func XPost(urlstr, qstr, qcstr, qtstr string, rr interface{}) (flag int, err error) {
+	flag = PostSuc
 	resp, err := http.PostForm(urlstr, url.Values{"qstr": {qstr}, "qcstr": {qcstr}, "qtstr": {qtstr}})
 	if err != nil {
 		log.Println("XPost post err:", err)
+		flag = PostERR
 		return
 	}
 	defer resp.Body.Close()
-	body, err = ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println("XPost ReadAll err:", err)
+		flag = ReadERR
 		return
+	}
+	err = UJSON(body, rr)
+	if err != nil {
+		flag = UJsonERR
 	}
 	return
 }
