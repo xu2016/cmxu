07f958553b489ad3e3fcbb89fa11470f9d66c297
xu2016 <451511298@qq.com>
2018-10-29 11:36:38 +0800

    l

--- xwb/session.go
+++ xwb/session.go
@@ -1,155 +1,24 @@
 package xwb
 
 import (
-	"cmxu/xcm"
-	"errors"
-	"net/http"
-	"sync"
 	"time"
 )
 
-//GSession 全局Session管理器
-var GSession *SessionManager
-
-//CookieName ...
-var CookieName = "sidx08063600"
-
-//然后在 init 函数中初始化
-func init() {
-	GSession, _ = NewSessionManager(CookieName, 3600)
-	go GSession.GC()
+//ZSession session存储结构
+type ZSession struct {
+	uid          string    //用户账号
+	timeAccessed time.Time //最后访问时间
+	gid          int       //群组ID
 }
 
-//ZSSession session存储结构
-type ZSSession struct {
-	Uname        string    //用户账号
-	TimeAccessed time.Time //最后访问时间
-	GID          int       //群组ID
-}
-
-//SessionManager Session管理器
-type SessionManager struct {
-	cookieName  string               //private cookiename
-	lock        sync.Mutex           // protects session
-	sid         map[string]ZSSession //session id 唯一标示
-	maxlifetime int64
-}
-
-//NewSessionManager 参加一个Session管理器
-func NewSessionManager(cookieName string, maxlifetime int64) (*SessionManager, error) {
-	sid := make(map[string]ZSSession, 0)
-	return &SessionManager{cookieName: cookieName, sid: sid, maxlifetime: maxlifetime}, nil
-}
-
-//GetSID 生成Session唯一ID
-func (sessionManager *SessionManager) GetSID(name string) (sid string) {
-	sid = xcm.GetMD5(name) + xcm.GetRandomString(8, xcm.NSDSTR)
+//GetUID 获取用户账号
+func (zs *ZSession) GetUID() (uid string) {
+	uid = zs.uid
 	return
 }
 
-//GC 定时对过期的Session进行删除
-func (sessionManager *SessionManager) GC() {
-	sessionManager.lock.Lock()
-	defer sessionManager.lock.Unlock()
-	for k, v := range sessionManager.sid {
-		if (v.TimeAccessed.Unix() + sessionManager.maxlifetime) < time.Now().Unix() {
-			delete(sessionManager.sid, k)
-		}
-	}
-	time.AfterFunc(time.Duration(sessionManager.maxlifetime*2), func() { sessionManager.GC() })
-}
-
-//Set 添加Session
-func (sessionManager *SessionManager) Set(sid, phone string, gid int) error {
-	sessionManager.lock.Lock()
-	defer sessionManager.lock.Unlock()
-	zs := ZSSession{}
-	zs.TimeAccessed = time.Now()
-	zs.GID = gid
-	zs.Uname = phone
-	sessionManager.sid[sid] = zs
-	return nil
-}
-
-//Get 获取Session
-func (sessionManager *SessionManager) Get(sid string) (zs ZSSession, err error) {
-	sessionManager.lock.Lock()
-	defer sessionManager.lock.Unlock()
-	zs, ok := sessionManager.sid[sid]
-	if !ok {
-		err = errors.New("no session")
-	}
-	return
-}
-
-//Del 删除Session
-func (sessionManager *SessionManager) Del(sid string) error {
-	sessionManager.lock.Lock()
-	defer sessionManager.lock.Unlock()
-	delete(sessionManager.sid, sid)
-	return nil
-}
-
-//Update 更新Session最后访问时间
-func (sessionManager *SessionManager) Update(sid string) error {
-	sessionManager.lock.Lock()
-	defer sessionManager.lock.Unlock()
-	zs, _ := sessionManager.sid[sid]
-	zs.TimeAccessed = time.Now()
-	sessionManager.sid[sid] = zs
-	return nil
-}
-
-//TimeOut 判断Session是否过期
-func (sessionManager *SessionManager) TimeOut(sid string) bool {
-	sessionManager.lock.Lock()
-	defer sessionManager.lock.Unlock()
-	if _, ok := sessionManager.sid[sid]; !ok {
-		return true
-	}
-	if (sessionManager.sid[sid].TimeAccessed.Unix() + sessionManager.maxlifetime) < time.Now().Unix() {
-		return true
-	}
-	return false
-}
-
-//GetCookie 获取客户端返回的Cookie
-func (sessionManager *SessionManager) GetCookie(r *http.Request, cookieName string) (sid string, err error) {
-	cookie, err := r.Cookie(cookieName)
-	if err != nil {
-		return
-	}
-	sid = cookie.Value
-	if sid == "" {
-		err = errors.New("sid is empty")
-		return
-	}
-	return
-}
-
-//SetCookie 设置客户端返回的Cookie
-func (sessionManager *SessionManager) SetCookie(w http.ResponseWriter, cookieName, sid string) {
-	cookie := http.Cookie{Name: cookieName, Value: sid, Path: "/", HttpOnly: true}
-	http.SetCookie(w, &cookie)
-	return
-}
-
-//DeleteCookie 设置客户端返回的Cookie
-func (sessionManager *SessionManager) DeleteCookie(w http.ResponseWriter, cookieName, sid string) {
-	cookie := http.Cookie{Name: cookieName, Value: sid, Path: "/", HttpOnly: true, MaxAge: -1}
-	http.SetCookie(w, &cookie)
-	return
-}
-
-//GetGID 获取群组ID
-func (sessionManager *SessionManager) GetGID(sid string) (gid int, err error) {
-	sessionManager.lock.Lock()
-	defer sessionManager.lock.Unlock()
-	zs, ok := sessionManager.sid[sid]
-	if !ok {
-		err = errors.New("no session")
-		return
-	}
-	gid = zs.GID
+//GetGID 获取用户账号
+func (zs *ZSession) GetGID() (gid int) {
+	gid = zs.gid
 	return
 }
---
+++ xwb/smanager.go
@@ -0,0 +1,134 @@
+package xwb
+
+import (
+	"cmxu/xcm"
+	"errors"
+	"net/http"
+	"sync"
+	"time"
+)
+
+//GSession 全局Session管理器
+var GSession *SManager
+
+//CName cookie名称
+var CName string
+
+//SManager Session管理器
+type SManager struct {
+	cookieName  string              //private cookiename
+	lock        sync.Mutex          // protects session
+	sid         map[string]ZSession //session id 唯一标示
+	maxlifetime int64
+}
+
+//NewSessionManager 参加一个Session管理器
+func NewSessionManager(cookieName string, maxlifetime int64) (*SManager, error) {
+	sid := make(map[string]ZSession, 0)
+	return &SManager{cookieName: cookieName, sid: sid, maxlifetime: maxlifetime}, nil
+}
+
+//createsid 生成Session唯一ID
+func (sm *SManager) createsid(name string) (sid string) {
+	sid = xcm.GetMD5(name) + xcm.GetRandomString(8, xcm.NSDSTR)
+	return
+}
+
+//GC 定时对过期的Session进行删除
+func (sm *SManager) GC() {
+	sm.lock.Lock()
+	defer sm.lock.Unlock()
+	for k, v := range sm.sid {
+		if (v.timeAccessed.Unix() + sm.maxlifetime) < time.Now().Unix() {
+			delete(sm.sid, k)
+		}
+	}
+	time.AfterFunc(time.Duration(sm.maxlifetime*2), func() { sm.GC() })
+}
+
+//Set 添加Session
+func (sm *SManager) Set(phone string, gid int) (sid string, err error) {
+	sm.lock.Lock()
+	defer sm.lock.Unlock()
+	sid = sm.createsid(phone)
+	if sid == "" {
+		err = errors.New("sid create error")
+		return
+	}
+	zs := ZSession{}
+	zs.timeAccessed = time.Now()
+	zs.gid = gid
+	zs.uid = phone
+	sm.sid[sid] = zs
+	return
+}
+
+//Get 获取Session
+func (sm *SManager) Get(sid string) (zs ZSession, err error) {
+	sm.lock.Lock()
+	defer sm.lock.Unlock()
+	zs, ok := sm.sid[sid]
+	if !ok {
+		err = errors.New("no session")
+	}
+	return
+}
+
+//Del 删除Session
+func (sm *SManager) Del(sid string) {
+	sm.lock.Lock()
+	defer sm.lock.Unlock()
+	delete(sm.sid, sid)
+	return
+}
+
+//Update 更新Session最后访问时间
+func (sm *SManager) Update(sid string) {
+	zs, _ := sm.sid[sid]
+	zs.timeAccessed = time.Now()
+	sm.lock.Lock()
+	defer sm.lock.Unlock()
+	sm.sid[sid] = zs
+	return
+}
+
+//TimeOut 判断Session是否过期
+func (sm *SManager) TimeOut(sid string) bool {
+	sm.lock.Lock()
+	defer sm.lock.Unlock()
+	if _, ok := sm.sid[sid]; !ok {
+		return true
+	}
+	if (sm.sid[sid].timeAccessed.Unix() + sm.maxlifetime) < time.Now().Unix() {
+		return true
+	}
+	return false
+}
+
+//GetCookie 获取客户端返回的Cookie
+func (sm *SManager) GetCookie(r *http.Request, cookieName string) (sid string, err error) {
+	cookie, err := r.Cookie(cookieName)
+	if err != nil {
+		return
+	}
+	sid = cookie.Value
+	if sid == "" {
+		err = errors.New("sid is empty")
+		return
+	}
+	return
+}
+
+//SetCookie 设置客户端返回的Cookie
+func (sm *SManager) SetCookie(w http.ResponseWriter, cookieName, sid string) {
+	cookie := http.Cookie{Name: cookieName, Value: sid, Path: "/", HttpOnly: true}
+	http.SetCookie(w, &cookie)
+	return
+}
+
+//DeleteCookie 设置客户端返回的Cookie
+func (sm *SManager) DeleteCookie(w http.ResponseWriter, cookieName, sid string) {
+	cookie := http.Cookie{Name: cookieName, Value: sid, Path: "/", HttpOnly: true, MaxAge: -1}
+	http.SetCookie(w, &cookie)
+	return
+}
