1b3f67ee7ac2271a94171810567ccc6c2445aea1
xu2016 <451511298@qq.com>
2018-12-03 18:07:54 +0800

    l

--- xuser/role.go
+++ xuser/role.go
@@ -18,7 +18,6 @@ type XMenu struct {
 	Xmid   string //菜单ID
 	Xrw    int    //排序序号
 	Xmname string //菜单名称
-	Xmtype string //菜单类型
 	Xmurl  string //URL地址
 	Xumid  string //上级菜单
 }
@@ -26,8 +25,8 @@ type XMenu struct {
 //LMenu 菜单链
 type LMenu struct {
 	Menu  *XMenu //我的菜单
-	CMenu *LMenu //首个子菜单链接
-	NMenu *LMenu //下一个兄弟菜单链接
+	CMenu *LMenu //子菜单链接
+	BMenu *LMenu //下一个兄弟菜单链接
 }
 
 //RoleMenu 角色菜单关系表
@@ -41,8 +40,22 @@ mdb:菜单数据库,mtb:菜单数据表, mtp:菜单数据库类型
 rdb:角色数据库, rtb:角色数据表, rtp:角色数据库类型
 rmdb:角色菜单关系数据库, rmtb:角色菜单关系数据表, rmtp:角色菜单关系数据库类型
 */
-func NewRole(mdb, mtb, mtp, rdb, rtb, rtp, rmdb, rmtb, rmtp string) (r []Role) {
-
+func NewRole(mdb, mtb, mtp, rdb, rtb, rtp, rmdb, rmtb, rmtp string) (r map[string]Role) {
+	//mm := readMenu(mdb, mtb, mtp)
+	rr := readRole(rdb, rtb, rtp)
+	rm := readRoleMenu(rmdb, rmtb, rmtp)
+	//m := menuLevel(mm)
+	for i, v := range rr {
+		rl := Role{}
+		rl.rname = v
+		for _, vv := range rm {
+			if vv.role == i {
+				rl.inlmt[vv.menu] = 1
+			}
+		}
+		//rl.menu = setRoleMenu(m, &rl)
+		r[i] = rl
+	}
 	return
 }
 
@@ -68,9 +81,9 @@ func (r *Role) GetInLmt(iid string) (b bool) {
 
 //readMenu 读取所有有效菜单
 func readMenu(mdb, mtb, mtp string) (menu map[string]XMenu) {
-	qstr := `select OBJID,RW,OBJNAME,OBJTYPE,CURL,UP_OBJID from ZSKDZS_OBJINFO where STATE='1'`
-	qcstr := `OBJID,RW,OBJNAME,OBJTYPE,CURL,UP_OBJID`
-	qtstr := `string,int,string,string,string,string`
+	qstr := `select OBJID,RW,OBJNAME,CURL,UP_OBJID from ZSKDZS_OBJINFO where STATE='1' and OBJTYPE='menu'`
+	qcstr := `OBJID,RW,OBJNAME,CURL,UP_OBJID`
+	qtstr := `string,int,string,string,string`
 	rr := xsql.QRowsJSON{}
 	rr.Init()
 	_, err := xwb.XPost("http://211.95.193.112:8080/sqlms/qrow", qstr, qcstr, qtstr, &rr)
@@ -87,7 +100,6 @@ func readMenu(mdb, mtb, mtp string) (menu map[string]XMenu) {
 			Xmid:   xsql.GetString(v.NmString["OBJID"], "null"),
 			Xrw:    int(xsql.GetInt64(v.NmInt["RW"], 0)),
 			Xmname: xsql.GetString(v.NmString["OBJNAME"], "null"),
-			Xmtype: xsql.GetString(v.NmString["OBJTYPE"], "null"),
 			Xmurl:  xsql.GetString(v.NmString["CURL"], "null"),
 			Xumid:  xsql.GetString(v.NmString["UP_OBJID"], "null"),
 		}
@@ -145,36 +157,17 @@ func readRoleMenu(rmdb, rmtb, rmtp string) (rolemenu []RoleMenu) {
 }
 
 //menuLevel 菜单分层
-func menuLevel(menu map[string]XMenu) (m *LMenu) {
-	xmenu := make(map[string]XMenu)
-	//按顺序添加主菜单
+func menuLevel(menu map[string]XMenu) (rm map[string][]XMenu) {
 	for i, v := range menu {
-		//判断是不是菜单
-		if v.Xmtype == "menu" {
-			//判断是不是主菜单
-			if v.Xumid == "null" {
-				xm := &LMenu{Menu: &v}
-				//如果主菜单为nil或主菜单第一个节点比新节点大
-				if m == nil || m.Menu.Xrw > v.Xrw {
-					xm.NMenu = m
-					m = xm
-				} else {
-					for p := m; p != nil; p = p.NMenu {
-						if p.Menu.Xrw < v.Xrw && (p.NMenu.Menu.Xrw > v.Xrw || p.NMenu == nil) {
-							xm.NMenu = p.NMenu
-							p.NMenu = xm
-							break
-						}
-					}
-				}
-			} else {
-				xmenu[i] = v
-			}
+		if v.Xumid == "null" {
+			rm[i] = append(rm[i], v)
 		}
 	}
-	//添加除主菜单外的所有子菜单
-	for {
+	return
+}
 
-	}
+//setRoleMenu 设置角色菜单
+func setRoleMenu(m *LMenu, rl *Role) (rm *LMenu) {
+
 	return
 }
