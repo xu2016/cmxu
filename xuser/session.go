a64f59a98cbee802d2e09d88479d112955eef5e3
xu2016 <451511298@qq.com>
2019-03-15 00:26:58 +0800

    l

--- xuser/session.go
+++ xuser/session.go
@@ -1,6 +1,8 @@
 package xuser
 
 import (
+	"errors"
+	"net/http"
 	"time"
 )
 
@@ -18,14 +20,64 @@ func (zs *ZSession) GetUID() (uid string) {
 	return
 }
 
-//GetGID 获取群组ID
-func (zs *ZSession) GetGID() (gid map[string]int) {
+//GetGIDS 获取群组IDS
+func (zs *ZSession) GetGIDS() (gid map[string]int) {
 	gid = zs.gid
+	return
+}
+
+//GetGID 获取特定群组ID
+func (zs *ZSession) GetGID(grptp string) (gid int) {
+	gid = zs.gid[grptp]
+	return
+}
+
+//UserContrl 用户权限控制
+func (zs *ZSession) UserContrl(grptp string, subMenuNum int) (b bool) {
+	if ((zs.gid[grptp] >> uint(subMenuNum)) & 1) == 1 {
+		b = true
+	}
 	return
 }
 
 //GetCity 获取用户所属地市
 func (zs *ZSession) GetCity() (city string) {
 	city = zs.city
+	return
+}
+
+//GetSession 获取并验证账号session是否存在、正确和未过期，
+//如果存在且正确并未过期，更新session,gid为用户角色（权限)。
+func GetSession(r *http.Request) (zs ZSession, err error) {
+	sid, err := GSession.GetCookie(r, CName)
+	if err != nil {
+		return
+	}
+	zs, err = GSession.Get(sid)
+	if err != nil {
+		return
+	}
+	if GSession.TimeOut(sid) {
+		err = errors.New("TimeOut")
+		return
+	}
+	GSession.Update(sid)
+	return
+}
+
+//DelSession 删除session
+func DelSession(w http.ResponseWriter, r *http.Request) {
+	sid, err := GSession.GetCookie(r, CName)
+	if err != nil {
+		return
+	}
+	GSession.Del(sid)
+	GSession.DeleteCookie(w, CName, sid)
+	return
+}
+
+//AddSession 添加session
+func AddSession(w http.ResponseWriter, sid string) {
+	GSession.SetCookie(w, CName, sid)
 	return
 }
